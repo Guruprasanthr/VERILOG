113b431b30eb999de70ec75d1ce5501ab0314f4c
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-04-09 08:35:51 +0530

    Create nandgate_tb.v

---
+++ Day 1/Logic Gates/nand gate/nandgate_tb.v
@@ -0,0 +1,16 @@
+module nandgate_tb;
+reg a,b;
+wire y;
+nandgate dut(.a(a),.b(b),.y(y));
+initial begin
+$dumpfile("waves.vcd");
+$dumpvars();
+end
+initial begin
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+$display("a=%b,b=%b,y=%b",a,b,y);
+end
+endmodule
