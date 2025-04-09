4cded4201853146518e34ae7c262f8973c0c30fa
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-04-09 08:38:51 +0530

    Create norgate_tb.v

---
+++ Day 1/Logic Gates/norgate/norgate_tb.v
@@ -0,0 +1,16 @@
+module norgate_tb;
+reg a,b;
+wire y;
+norgate dut(.a(a),.b(b),.y(y));
+initial begin
+$dumpfile("waves.vcd");
+$dumpvars();
+end
+initial begin
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+$display("a=%b,b=%b,y=%b",a,b,y);
+end
+endmodule
