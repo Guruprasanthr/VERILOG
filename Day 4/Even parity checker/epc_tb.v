5371faa9d12799248f65bf45fc52525563b26d10
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-04-17 15:41:27 +0530

    Update epc_tb.v

--- Day 4/Even parity checker/epc_tb.v
+++ Day 4/Even parity checker/epc_tb.v
@@ -1,18 +1,26 @@
 module epc_tb;
-reg a,b,c;
+reg a,b,c,d;
 wire x;
-epc uut(.a(a),.b(b),.c(c),.x(x));
+  epc uut(.a(a),.b(b),.c(c),.d(d),.x(x));
 initial begin
-$monitor("a=%b,b=%b,c=%b,x=%b",a,b,c,x);
+  $monitor("a=%b,b=%b,c=%b,d=%b,x=%b",a,b,c,x);
 $dumpfile("waves.vcd");
 $dumpvars();
-a=0; b=0; c=0; #10;
-a=0; b=0; c=1; #10;
-a=0; b=1; c=0; #10;
-a=0; b=1; c=1; #10;
-a=1; b=0; c=0; #10;
-a=1; b=0; c=1; #10;
-a=1; b=1; c=0; #10;
-a=1; b=1; c=1; #10;
+#5 a=0; b=0; c=0; p=0; 
+#5 a=0; b=0; c=0; p=1; 
+#5 a=0; b=0; c=1; p=0; 
+#5 a=0; b=0; c=1; p=1; 
+#5 a=0; b=1; c=0; p=0; 
+#5 a=0; b=1; c=0; p=1; 
+#5 a=0; b=1; c=1; p=0; 
+#5 a=0; b=1; c=1; p=1; 
+#5 a=1; b=0; c=0; p=0; 
+#5 a=1; b=0; c=0; p=1; 
+#5 a=1; b=0; c=1; p=0; 
+#5 a=1; b=0; c=1; p=1; 
+#5 a=1; b=1; c=0; p=0; 
+#5 a=1; b=1; c=0; p=1; 
+#5 a=1; b=1; c=1; p=0; 
+#5 a=1; b=1; c=1; p=1; 
 end
 endmodule
