48a6887b3cc974b6b6eddfd598f005f5dcca58e9
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-04-23 16:23:46 +0530

    Create gb_tb.v

---
+++ Day 5/Code convertors/Gray to binary/gb_tb.v
@@ -0,0 +1,14 @@
+module gb_tb;
+reg [3:0] a;
+wire [3:0] b;
+gb dut(.a(a),.b(b));
+initial begin
+$monitor("a=%b,b=%b",a,b);
+$dumpfile("waves.vcd");
+$dumpvars();
+a=4'b0000;
+#5 a=4'b0001;
+#5 a=4'b0011;
+#5 a=4'b0010;
+end
+endmodule
