fecd768a667735256c4f0948bd14ff74ab0a306a
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-05-07 09:29:48 +0530

    Create mux2.v

---
+++ Assignments/1.Implement a 2:1 multiplexer using the ? operator./mux2.v
@@ -0,0 +1,23 @@
+//design
+module mux2to1 (
+input wire a, b,    
+input wire sel,         
+output wire out);
+assign out = sel ? b : a;
+endmodule
+//testbench
+module mux2to1_tb;
+reg a, b, sel;
+wire out;
+mux2to1 uut (.a(a), .b(b), .sel(sel), .out(out));
+initial begin
+$monitor(" a=%b,b=%b,sel=%b,out=%b",a, b, sel, out);
+$dumpfile("waves.vcd");
+$dumpvars();
+a = 0; b = 0; sel = 0;
+#10 a = 1; sel = 0;
+#10 b = 1; sel = 1; 
+#10 a = 0; sel = 0;
+#10 b = 0; sel = 1; 
+end
+endmodule
