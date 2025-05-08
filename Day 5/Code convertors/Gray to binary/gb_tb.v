0294e99d3f50b4fe9fd74c59705c2e676014225e
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-05-08 12:24:06 +0530

    Update gb_tb.v

--- Day 5/Code convertors/Gray to binary/gb_tb.v
+++ Day 5/Code convertors/Gray to binary/gb_tb.v
@@ -12,3 +12,22 @@ a=4'b0000;
 #5 a=4'b0010;
 end
 endmodule
+
+//
+module g2b_tb;
+reg[3:0]g;
+wire[3:0]b;
+g2b dut(.g(g),.b(b));
+initial begin
+$monitor("g=%b,b=%b",g,b);
+$dumpfile("waves.vcd");
+$dumpvars();
+ g = 4'b0000; #10;
+ g = 4'b0001; #10;
+ g = 4'b0011; #10;
+ g = 4'b0100; #10;
+ g = 4'b1010; #10;
+ g = 4'b0111; #10;
+ g = 4'b1111; #10;
+end
+endmodule
