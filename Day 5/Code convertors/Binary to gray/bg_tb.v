19993b7009983987d7e2a1da9aa9c91e3d902202
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-05-08 12:00:05 +0530

    Update bg_tb.v

--- Day 5/Code convertors/Binary to gray/bg_tb.v
+++ Day 5/Code convertors/Binary to gray/bg_tb.v
@@ -12,3 +12,21 @@ a=4'b0000;
 #5 a=4'b0100;
 end
 endmodule
+
+//module b2g_tb;
+reg[3:0]b;
+wire[3:0]g;
+b2g dut(.b(b),.g(g));
+initial begin
+$monitor("b=%b,g=%b",b,g);
+$dumpfile("waves.vcd");
+$dumpvars();
+ b = 4'b0000; #10;
+ b = 4'b0001; #10;
+ b = 4'b0010; #10;
+ b = 4'b0110; #10;
+ b = 4'b1010; #10;
+ b = 4'b1110; #10;
+ b = 4'b1111; #10;
+end
+endmodule
