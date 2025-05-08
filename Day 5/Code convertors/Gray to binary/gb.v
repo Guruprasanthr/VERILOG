1774fc51a696aa492a7893d22edfd8aad8442882
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-05-08 12:24:36 +0530

    Update gb.v

--- Day 5/Code convertors/Gray to binary/gb.v
+++ Day 5/Code convertors/Gray to binary/gb.v
@@ -1,3 +1,12 @@
 module gb(input[3:0] a,output[3:0] b);
 assign b=a^(b>>1);
 endmodule
+//
+module g2b (input  [3:0] g,output reg [3:0] b);
+always @(*) begin
+    b[3] = g[3];
+    b[2] = g[3] ^ g[2];
+    b[1] = g[3]^g[2] ^ g[1];
+    b[0] = g[3]^g[2]^g[1] ^ g[0];
+end
+endmodule
