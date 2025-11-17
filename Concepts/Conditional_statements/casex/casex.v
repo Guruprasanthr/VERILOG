15f9a4b64a02c4a8b66c86fe0a3f657fab6fe2b4
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-11-17 10:48:12 +0530

    Create casex.v

---
+++ Concepts/Conditional_statements/casex/casex.v
@@ -0,0 +1,9 @@
+module casex_example(input [3:0] in, output reg [1:0] out);
+  always @(*) begin
+    casex(in)
+      4'b1x00: out = 2'b01;  
+      4'b01x0: out = 2'b10;  
+      default: out = 2'b00;
+    endcase
+  end
+endmodule
